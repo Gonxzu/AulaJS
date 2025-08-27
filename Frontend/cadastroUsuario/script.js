2b6d2d0d3ead49c67c6791b9633af7e035be4623
otavionvs <92130086+otavionvs@users.noreply.github.com>
2025-08-27 16:55:55 -0300

    Fetch post

---
+++ Frontend/cadastroUsuario/script.js
@@ -0,0 +1,32 @@
+
+function cadastroUsuario(event) {
+    event.preventDefault();
+
+    let nome = event.target.nome.value;
+    let idade = event.target.idade.value;
+    let senha = event.target.senha.value;
+
+    fetch('http://localhost:3000/usuarios', {
+
+        method: 'POST',
+
+        headers: {
+
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({
+            "nome": nome,
+            "idade": idade,
+            "senha": senha
+        })
+
+    })
+
+        .then(response => response.json())
+
+        .then(dados => console.log(dados))
+
+        .catch(error => console.log(error));
+}
